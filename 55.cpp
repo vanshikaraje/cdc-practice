8d0f912fd6f1d2a774ec8cb55d60d406f1cc011b
Vanshika <vanshikaraje010@gmail.com>
2025-10-16 19:51:20 +0530

    Add files via upload

---
+++ 55.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
+        }
+        while (j >= 0) nums1[k--] = nums2[j--];
+    }
+};
+
+int main() {
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    int m = 3, n = 3;
+    
+    Solution().merge(nums1, m, nums2, n);
+    
+    for (int num : nums1) cout << num << " ";
+    return 0;
+}
