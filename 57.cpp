abe803e7e388023cb72ff002cac03fc6aa93aac3
Vanshika <vanshikaraje010@gmail.com>
2025-10-16 19:52:26 +0530

    Add files via upload

---
+++ 57.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size() - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else { // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    Solution sol;
+    sol.sortColors(nums);
+
+    for (int num : nums)
+        cout << num << " ";
+
+    return 0;
+}
