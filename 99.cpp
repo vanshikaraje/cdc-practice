50b0a0b6c24933b36ac2b366dbecf956b912b772
Vanshika <vanshikaraje010@gmail.com>
2025-10-21 21:13:05 +0530

    Add files via upload

---
+++ 99.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Detect intersection point in the cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find the entrance (duplicate number)
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
