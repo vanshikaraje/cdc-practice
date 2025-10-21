616b875bd6bfc46474d0543d6dce84d9e44e04f1
Vanshika <vanshikaraje010@gmail.com>
2025-10-21 21:12:33 +0530

    Add files via upload

---
+++ 97.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    vector<int> result = sol.countBits(n);
+    cout << "Output: [";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+        if (i != result.size() - 1) cout << ",";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
