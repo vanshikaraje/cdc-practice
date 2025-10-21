f8013bd63a6f80727990158e065f55f87f423688
Vanshika <vanshikaraje010@gmail.com>
2025-10-21 21:08:49 +0530

    Add files via upload

---
+++ 93.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) return 0;
+        int sum = 0;
+
+        // Check if left child exists and is a leaf
+        if (root->left && !root->left->left && !root->left->right) {
+            sum += root->left->val;
+        }
+
+        // Recur for left and right subtree
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
